db77dcfaac4d7a191022c0bdbd1edeee9e921681
Pablo Pudgino <pavel.ivanov@serenedb.com>
2026-02-05 20:03:42 +0100

    perf: Remove keys copying in SSTWriters

--- include/rocksdb/sst_file_writer.h
+++ include/rocksdb/sst_file_writer.h
@@ -77,7 +77,6 @@ class SstFileWriter {
  public:
   // Internal key footer for SST files: PackSequenceAndType(0, kTypeValue)
   // The footer is 8 bytes encoding (sequence_number << 8) | value_type
-  // kTypeValue = 0x1, verified by static_assert in sst_file_writer.cc
   static constexpr uint64_t kInternalKeyFooter = (uint64_t{0} << 8) | 0x1;
 
   // User can pass `column_family` to specify that the generated file will
